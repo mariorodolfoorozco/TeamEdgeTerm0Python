cfb1cbeb148d335979b69086c57dc226680fa5e7
mariorodolfoorozco <mario.orozco@csedge.org>
2022-07-06 14:51:43 -0700

    Day 1

--- variables.py
+++ variables.py
@@ -16,6 +16,10 @@ print(message)
 
 print("------------------- Challenge 1 -------------------")
 
+name = "Mario"
+grade = 11
+funFact = "I like to play videogames"
+
 print(f"Hi! My name is {name}")
 print(f"I'm in the {grade}th grade.")
 print(f"A fun fact about me is that {funFact}")
@@ -29,14 +33,24 @@ print(f"A fun fact about me is that {funFact}")
 # **** Upchallenge! ****
 # Can you also print out the data type of the variables? 
 
-
+print(type(name))
+print(type(grade))
+print(type(funFact))
 
 
 # **** Challenge 1: Problem 2 ****
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
 
+line_1 = "Happy Birthday to you!"
+line_2 = "Happy Birthday to you!"
+line_3 = "Happy Birthday to Mario!"
+line_4 = "Happy Birthday to you!"
+line_5 = "And many mooooore!"
 
+
+
+print(f"{line_1} {line_2} {line_3} {line_4} {line_5}")
 
 
 # **** Challenge 1: Problem 3 ****
@@ -57,35 +71,50 @@ print("------------------- Challenge 2 -------------------")
 num1 = 5
 num2 = 10
 
+
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
 
+sum = num1 + num2
+print(sum)
 
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
+
+sum2 = sum - 7
+print(sum2)
 
 
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
 
+sum3 = sum2 * 3
+print(sum3)
 
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
+
+sum4 = sum3 ** 2
+print(sum4)
 
 
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
 
+sum5 = sum4 / 4
+print(sum5)
 
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
 
+sum6 = sum5 % 2
+print(sum6)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
@@ -93,5 +122,10 @@ num2 = 10
 # Your code should work if the value of the variable is changed.
 
 num = 9
+before = num - 1
+after = num + 1
+
+print(before)
+print(after)
 
 # -------------------------------------------- 
